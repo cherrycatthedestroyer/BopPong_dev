8914b061c13616c4e7ecf7fe65a32a9eb3195d85
cherrycatthedestroyer <josh96j@gmail.com>
2023-02-20 03:44:29 +0400

    added spotify functionality . song search and retrieval via api . song playback via spotify player nonintrusive

--- app/src/main/java/com/example/boppong_dev/MainActivity.java
+++ app/src/main/java/com/example/boppong_dev/MainActivity.java
@@ -2,15 +2,101 @@ package com.example.boppong_dev;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+
+import com.example.boppong_dev.Connectors.UserService;
+import com.example.boppong_dev.Model.User;
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.spotify.sdk.android.auth.AuthorizationClient;
+import com.spotify.sdk.android.auth.AuthorizationRequest;
+import com.spotify.sdk.android.auth.AuthorizationResponse;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String CLIENT_ID = "a24f9f02a4fc4adb8138143d99bd8dc9";
+    private static final String REDIRECT_URI = "https://www.youtube.com/";
+    private SpotifyAppRemote mSpotifyAppRemote;
+
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest;
+    private String url = "https://api.spotify.com/v1/search?q=hair%20down&type=track&limit=3&offset=5";
+
+    private static final int REQUEST_CODE = 1337;
+    private SharedPreferences.Editor editor;
+    private SharedPreferences msharedPreferences;
+    AuthorizationRequest.Builder builder =
+            new AuthorizationRequest.Builder(CLIENT_ID, AuthorizationResponse.Type.TOKEN, REDIRECT_URI);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        builder.setScopes(new String[]{"streaming"});
+        AuthorizationRequest request = builder.build();
+        AuthorizationClient.openLoginActivity(this, REQUEST_CODE, request);
+        msharedPreferences = this.getSharedPreferences("SPOTIFY", 0);
+        mRequestQueue = Volley.newRequestQueue(this);
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
 
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthorizationResponse response = AuthorizationClient.getResponse(resultCode, intent);
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    editor = getSharedPreferences("SPOTIFY", 0).edit();
+                    editor.putString("token", response.getAccessToken());
+                    Log.d("STARTING", "GOT AUTH TOKEN");
+                    editor.apply();
+                    waitForUserInfo();
+                    System.out.println("working");
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    System.out.println("bad auth");
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    // Handle other cases
+            }
+        }
+    }
+
+    private void waitForUserInfo() {
+        UserService userService = new UserService(mRequestQueue, msharedPreferences);
+        userService.get(() -> {
+            User user = userService.getUser();
+            editor = getSharedPreferences("SPOTIFY", 0).edit();
+            editor.putString("userid", user.id);
+            Log.d("STARTING", "GOT USER INFORMATION");
+            // We use commit instead of apply because we need the information stored immediately
+            editor.commit();
+            startActivity2();
+        });
+    }
+
+    private void startActivity2() {
+        Intent newIntent = new Intent(MainActivity.this, Activity2.class);
+        startActivity(newIntent);
+    }
 }
